552fd6fdb5b84828e29309b3274611c9de8f2e75
arzakaraffan <arzakaraffan@gmail.com>
2025-03-26 13:32:40 +0700

    Initial Commit

---
+++ src/main/java/id/cs/ui/advprog/inthecost/InthecostApplication.java
@@ -0,0 +1,13 @@
+package id.cs.ui.advprog.inthecost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InthecostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InthecostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/cs/ui/advprog/inthecost/InthecostApplicationTests.java
@@ -0,0 +1,13 @@
+package id.cs.ui.advprog.inthecost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InthecostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
