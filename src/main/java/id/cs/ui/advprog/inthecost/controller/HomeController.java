9093ed2e64d8ac684f7fab5385dd330d4c5ae86c
Brian Altan <brianaltan28@gmail.com>
2025-04-10 23:20:56 +0700

    add preview title

---
+++ src/main/java/id/cs/ui/advprog/inthecost/controller/HomeController.java
@@ -0,0 +1,13 @@
+package id.cs.ui.advprog.inthecost;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to InTheKost!";
+    }
+}
