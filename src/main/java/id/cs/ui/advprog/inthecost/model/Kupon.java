62cb5273ca51d831bf622dde2e28c1a72f1b0407
arzakaraffan <arzakaraffan@gmail.com>
2025-04-11 02:52:52 +0700

    [GREEN] Implement Kupon model

--- src/main/java/id/cs/ui/advprog/inthecost/model/Kupon.java
+++ src/main/java/id/cs/ui/advprog/inthecost/model/Kupon.java
@@ -16,6 +16,17 @@ public class Kupon{
     private boolean kuponGlobal;
 
     public Kupon(String pemilik, LocalDate masaBerlaku, int persentase, String deskripsi, boolean kuponGlobal) {
+        this.idKupon = UUID.randomUUID().toString();
+        this.pemilik = pemilik;
+        this.kodeUnik = generateKodeUnik();
+        this.persentase = persentase;
+        this.masaBerlaku = masaBerlaku;
+        this.deskripsi = deskripsi;
+        this.kuponGlobal = kuponGlobal;
+    }
+
+    private String generateKodeUnik(){
+        return UUID.randomUUID().toString().substring(0,6).toUpperCase();
     }
 
 }
