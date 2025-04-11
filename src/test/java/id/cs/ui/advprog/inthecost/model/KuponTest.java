13540c5939edf0e80add7370447efdc4c1d6d5e9
arzakaraffan <arzakaraffan@gmail.com>
2025-04-11 15:06:53 +0700

    [RED] Adding More Test for Refactored Kupon Model

--- src/test/java/id/cs/ui/advprog/inthecost/model/KuponTest.java
+++ src/test/java/id/cs/ui/advprog/inthecost/model/KuponTest.java
@@ -1,5 +1,6 @@
 package id.cs.ui.advprog.inthecost.model;
 
+import id.cs.ui.advprog.inthecost.enums.KuponStatus;
 import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,8 +15,8 @@ public class KuponTest {
     void setUp(){
         String pemilik = "Admin";
         int persentase = 10;
-        LocalDate masaBerlaku = LocalDate.of(2025, 4, 10);
-        String deskripsi = "Kupon Lebaran Idul Fitri 2025";
+        LocalDate masaBerlaku = LocalDate.of(2026, 10, 10);
+        String deskripsi = "Kupon Lebaran Idul Fitri 2026";
         boolean kuponGlobal = true;
 
         this.kupon = new Kupon(pemilik, masaBerlaku, persentase, deskripsi,kuponGlobal);
@@ -31,10 +32,10 @@ public class KuponTest {
     void testGetPersentase(){assertEquals(10, this.kupon.getPersentase());}
 
     @Test
-    void testGetMasaBerlakuKupon(){assertEquals(LocalDate.of(2025, 4, 10), this.kupon.getMasaBerlaku());}
+    void testGetMasaBerlakuKupon(){assertEquals(LocalDate.of(2026, 10, 10), this.kupon.getMasaBerlaku());}
 
     @Test
-    void testGetDeskripsiKupon(){assertEquals("Kupon Lebaran Idul Fitri 2025", this.kupon.getDeskripsi());}
+    void testGetDeskripsiKupon(){assertEquals("Kupon Lebaran Idul Fitri 2026", this.kupon.getDeskripsi());}
 
     @Test
     void testGetStatusKuponGlobal(){assertTrue(this.kupon.isKuponGlobal());}
@@ -44,4 +45,74 @@ public class KuponTest {
 
     @Test
     void testKodeUnikKuponNotNull(){assertNotNull(this.kupon.getKodeUnik());}
+
+    @Test
+    void testPemilikNull(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            Kupon newKupon = new Kupon(null, LocalDate.of(2026, 4, 10), 10, "Kupon Lebaran Idul Fitri 2026", true);
+        });
+    }
+
+    @Test
+    void testMasaBerlakuNull(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            Kupon newKupon = new Kupon("Admin", null, 10, "Kupon Lebaran Idul Fitri 2026", true);
+        });
+    }
+
+    @Test
+    void testPersentaseInvalid(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            Kupon newKupon = new Kupon("Admin", LocalDate.of(2026, 4, 10), -30, "Kupon Lebaran Idul Fitri 2026", true);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            Kupon newKupon = new Kupon("Admin", LocalDate.of(2026, 4, 10), 0, "Kupon Lebaran Idul Fitri 2026", true);
+        });
+    }
+
+    @Test
+    void testDeskripsiNull(){
+        assertThrows(IllegalArgumentException.class, ()->{
+            Kupon newKupon = new Kupon("Admin", LocalDate.of(2026, 4, 10), 10, null, true);
+        });
+        assertThrows(IllegalArgumentException.class, ()->{
+            Kupon newKupon = new Kupon("Admin", LocalDate.of(2026, 4, 10), 10, "", true);
+        });
+    }
+
+    @Test
+    void testSetPersentaseInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> kupon.setPersentase(0));
+        assertThrows(IllegalArgumentException.class, () -> kupon.setPersentase(200));
+    }
+
+    @Test
+    void testSetDeskripsiInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> kupon.setDeskripsi(""));
+        assertThrows(IllegalArgumentException.class, () -> kupon.setDeskripsi(null));
+    }
+
+    @Test
+    void testSetMasaBerlakuInvalid() {
+        assertThrows(IllegalArgumentException.class, () -> kupon.setMasaBerlaku(null));
+    }
+
+    @Test
+    void testStatusKuponValid(){
+        System.out.println(kupon.getStatusKupon());
+        kupon.refreshStatus();
+        assertEquals(KuponStatus.VALID, kupon.getStatusKupon());
+    }
+
+    @Test
+    void testStatusKuponInvalid(){
+        kupon.setMasaBerlaku(LocalDate.of(2020, 1, 1));
+        assertEquals(KuponStatus.INVALID, kupon.getStatusKupon());
+    }
+
+    @Test
+    void testKuponToString(){
+        String kodeUnik = kupon.getKodeUnik();
+        assertEquals(String.format("Kupon[%s, Admin, 10%%, Hingga: 10 October 2026, Status: VALID]", kodeUnik), kupon.toString());
+    }
 }
