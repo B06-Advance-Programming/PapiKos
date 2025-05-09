1b25608d3c5438561598e5c73a9ded6028fc9e3d
RayhanSP <ray.han2005.rh@gmail.com>
2025-05-09 20:15:46 +0700

    [REFACTOR] Remove database connection test for previous dummy db

--- src/main/java/id/cs/ui/advprog/inthecost/config/DatabaseConfig.java
+++ src/main/java/id/cs/ui/advprog/inthecost/config/DatabaseConfig.java
@@ -1,24 +1,24 @@
-package id.cs.ui.advprog.inthecost.config;
+// package id.cs.ui.advprog.inthecost.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+// import io.github.cdimascio.dotenv.Dotenv;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.boot.jdbc.DataSourceBuilder;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-@Configuration
-public class DatabaseConfig {
+// @Configuration
+// public class DatabaseConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        Dotenv dotenv = Dotenv.configure().load();
+//     @Bean
+//     public DataSource dataSource() {
+//         Dotenv dotenv = Dotenv.configure().load();
 
-        return DataSourceBuilder.create()
-                .url("jdbc:postgresql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME"))
-                .username(dotenv.get("DB_USER"))
-                .password(dotenv.get("DB_PASSWORD"))
-                .driverClassName("org.postgresql.Driver")
-                .build();
-    }
-}
+//         return DataSourceBuilder.create()
+//                 .url("jdbc:postgresql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME"))
+//                 .username(dotenv.get("DB_USER"))
+//                 .password(dotenv.get("DB_PASSWORD"))
+//                 .driverClassName("org.postgresql.Driver")
+//                 .build();
+//     }
+// }
--- src/test/java/id/cs/ui/advprog/inthecost/repository/DatabaseConnectionTest.java
+++ src/test/java/id/cs/ui/advprog/inthecost/repository/DatabaseConnectionTest.java
@@ -1,24 +1,24 @@
-package id.cs.ui.advprog.inthecost.repository;
+// package id.cs.ui.advprog.inthecost.repository;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.sql.DataSource;
+// import javax.sql.DataSource;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+// import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
-public class DatabaseConnectionTest {
+// @SpringBootTest
+// public class DatabaseConnectionTest {
 
-    @Autowired
-    private DataSource dataSource;
+//     @Autowired
+//     private DataSource dataSource;
 
-    @Test
-    void testDatabaseConnection() throws Exception {
-        assertNotNull(dataSource, "DataSource should not be null");
-        try (var connection = dataSource.getConnection()) {
-            assertNotNull(connection, "Database connection should be established");
-        }
-    }
-}
+//     @Test
+//     void testDatabaseConnection() throws Exception {
+//         assertNotNull(dataSource, "DataSource should not be null");
+//         try (var connection = dataSource.getConnection()) {
+//             assertNotNull(connection, "Database connection should be established");
+//         }
+//     }
+// }
