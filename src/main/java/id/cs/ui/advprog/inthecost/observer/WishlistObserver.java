04fe3d66b2d0e46b552043b0fa557de48740c18d
RayhanSP <ray.han2005.rh@gmail.com>
2025-05-09 17:55:09 +0700

    [GREEN] WishlistObserver passing unit test

--- src/main/java/id/cs/ui/advprog/inthecost/observer/WishlistObserver.java
+++ src/main/java/id/cs/ui/advprog/inthecost/observer/WishlistObserver.java
@@ -2,18 +2,22 @@ package id.cs.ui.advprog.inthecost.observer;
 
 import id.cs.ui.advprog.inthecost.model.Kost;
 import id.cs.ui.advprog.inthecost.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class WishlistObserver implements Observer {
 
     private final NotificationService notificationService;
 
+    public WishlistObserver(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
     @Override
     public void update(Object subject) {
-        if (subject instanceof Kost) {
-            Kost kost = (Kost) subject;
-            notificationService.notifyUsers(kost);
+        if (subject instanceof Kost kost) {
+            // Only notify users if the Kost has available rooms
+            if (kost.getJumlahKamar() > 0) {
+                notificationService.notifyUsers(kost);
+            }
         }
     }
 }
