7a55a5a2f03f7f0f544e89bd142b14a987ece1d3
RayhanSP <ray.han2005.rh@gmail.com>
2025-05-22 19:28:37 +0700

    Fix merge error in InthecostApplication.java

--- src/main/java/id/cs/ui/advprog/inthecost/InthecostApplication.java
+++ src/main/java/id/cs/ui/advprog/inthecost/InthecostApplication.java
@@ -1,18 +1,24 @@
 package id.cs.ui.advprog.inthecost;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import id.cs.ui.advprog.inthecost.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class InthecostApplicationTests {
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
-    @Test
-    void contextLoads() {
+@SpringBootApplication
+public class InthecostApplication {
+
+    @PostConstruct
+    public void init() {
+        // Set timezone ke Asia/Jakarta (WIB)
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Jakarta"));
+        System.out.println("Timezone set to: " + TimeZone.getDefault().getID());
     }
 
-//    @Test
-//    void mainMethodTest(){
-//        InthecostApplication.main(new String[]{});
-//    }
+    public static void main(String[] args) {
+        SpringApplication.run(InthecostApplication.class, args);
+    }
 
 }
