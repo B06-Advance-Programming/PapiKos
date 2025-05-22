274d24a8e97fa64211255c56cd6ded9f1144c75d
Attar <attaralwie@gmail.com>
2025-05-23 04:27:29 +0700

    feat: trying to implement jwt

--- src/main/java/id/cs/ui/advprog/inthecost/config/ApplicationConfiguration.java
+++ src/main/java/id/cs/ui/advprog/inthecost/config/ApplicationConfiguration.java
@@ -1,10 +1,13 @@
 package id.cs.ui.advprog.inthecost.config;
 
+
 import id.cs.ui.advprog.inthecost.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,6 +29,11 @@ public class ApplicationConfiguration {
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
---
+++ src/main/java/id/cs/ui/advprog/inthecost/dto/LoginUserDto.java
@@ -0,0 +1,14 @@
+package id.cs.ui.advprog.inthecost.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginUserDto {
+
+    private String email;
+
+    private String password;
+
+}
