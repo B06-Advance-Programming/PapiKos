12ce78f94514ec90e4616b9be42595e57b603560
RayhanSP <ray.han2005.rh@gmail.com>
2025-05-23 03:13:55 +0700

    [GREEN] Update WishlistService interface with new method signatures

--- src/main/java/id/cs/ui/advprog/inthecost/service/WishlistService.java
+++ src/main/java/id/cs/ui/advprog/inthecost/service/WishlistService.java
@@ -18,4 +18,37 @@ public interface WishlistService {
      * @return List of Kost objects that the user has added to their wishlist
      */
     List<Kost> getWishlistByUserId(UUID userId);
+    
+    /**
+     * Add a kost to a user's wishlist
+     * 
+     * @param userId The unique identifier of the user
+     * @param kostId The unique identifier of the kost
+     */
+    void addToWishlist(UUID userId, UUID kostId);
+    
+    /**
+     * Remove a kost from a user's wishlist
+     * 
+     * @param userId The unique identifier of the user
+     * @param kostId The unique identifier of the kost
+     */
+    void removeFromWishlist(UUID userId, UUID kostId);
+    
+    /**
+     * Check if a kost is in a user's wishlist
+     * 
+     * @param userId The unique identifier of the user
+     * @param kostId The unique identifier of the kost
+     * @return true if the kost is in the user's wishlist, false otherwise
+     */
+    boolean isInWishlist(UUID userId, UUID kostId);
+    
+    /**
+     * Count how many users have added a specific kost to their wishlist
+     * 
+     * @param kostId The unique identifier of the kost
+     * @return The number of users who have added the kost to their wishlist
+     */
+    int countWishlistsByKostId(UUID kostId);
 }
